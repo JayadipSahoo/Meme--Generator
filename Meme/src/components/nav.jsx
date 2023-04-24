73825dc538887d8927e1f7af5453c8462509fad2
JayadipSahoo <jayadip360@gmail.com>
2023-04-25 01:10:08 +0530

    !st

---
+++ Meme/src/components/nav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import img2 from '../assets/logo2.png'
+
+ export default function nav(){
+    return (<div 
+         className="Navigation">
+            <img src={img2}></img>
+            <h1>MEME GENERATOR</h1>
+    </div>)
+}
